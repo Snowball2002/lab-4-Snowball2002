645dfecc55bbe21ae1504c03af182be6d86386aa
Snowball2002 <124380575+Snowball2002@users.noreply.github.com>
2024-10-08 19:51:38 -0400

    Initial Commit

---
+++ lab_4_paolo/lab_4_paoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab_4_paoloApp.swift
+//  lab_4_paolo
+//
+//  Created by drew mclean on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct lab_4_paoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lab_4_paoloTests/lab_4_paoloTests.swift
@@ -0,0 +1,17 @@
+//
+//  lab_4_paoloTests.swift
+//  lab_4_paoloTests
+//
+//  Created by drew mclean on 10/8/24.
+//
+
+import Testing
+@testable import lab_4_paolo
+
+struct lab_4_paoloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
